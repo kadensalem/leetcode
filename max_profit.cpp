2525e1e3746e765709745e0c0e0ea21db85a6c8d
Kaden Salem <kaden@salemfamily.com>
2023-10-12 19:56:54 -0600

    Solved max profit

---
+++ max_profit.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            else if (i == prices.size() - 1) {
+                if (prices[i] - min > 0) {
+                    profit += prices[i] - min;
+                }
+            }
+            else if (prices[i + 1] < prices[i]) {
+                profit += (prices[i] - min);
+                min = prices[i + 1];
+                i++;
+            }
+        }
+        return profit;
+        
+    }
+};
